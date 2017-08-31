94db78c93b02e6307d630a830358c6b636ad4534
Jiaqi Wang <jackyjqw@gmail.com>
2017-08-31 03:21:19 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title class = "en">Jiaqi Wang</title>
+<title class = "cn">王嘉琪</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" /> 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+<script src="jquery.youtubepopup.min.js"></script>
+<script src="js/PopupYoutubeVideo.js"></script>
+<!--<script src="js/IP.js"></script>-->
+<link rel = "stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/redmond/jquery-ui.css" rel="stylesheet" />
+<link rel = "stylesheet" type = "text/css" href = "CSS/ImageHoverEffect.css">
+<link rel = "stylesheet" type = "text/css" href = "CSS/Page2.css">
+<link rel = "stylesheet" type = "text/css" href = "CSS/PopupVideo.css">
+
+<link rel="shortcut icon" href="image/JW.gif">
+</head>
+
+<body class = "en">
+
+    <!--<div id="LeftCircle"><img src="image/myshadow1.jpg"></div>-->
+    <div id="MainSect">
+            <?php include 'php/language.php';?>
+	    <?php include 'php/myinfo.php';?>
+ 	    <?php include 'php/ProjectsContainer.php';?>
+ 	    <!--<?php include 'php/IP.php';?>-->
+    </div>
+    <?php include 'php/footer.php';?>
+</body>
+
+</html>
