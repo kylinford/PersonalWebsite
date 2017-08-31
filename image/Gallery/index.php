7b0048efa6931ed3cf688b4a0f91fc15d88c0f89
Jiaqi Wang <jackyjqw@gmail.com>
2017-08-31 03:09:01 -0700

    Add files via upload

---
+++ image/Gallery/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+<title class = "en">Jiaqi Wang</title>
+<title class = "cn">王嘉琪</title>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" /> 
+<link rel = "stylesheet" type = "text/css" media = "screen" href = "../../CSS/ImageHoverEffect.css">
+<link rel = "stylesheet" type = "text/css" media = "screen" href = "../../CSS/Page.css">
+<link rel="shortcut icon" href="image/JW.gif">
+
+</head>
+
+<body class = "en">
+
+<?php include '../../php/header.php';?>
+
+<center>
+	<?php include '../../php/LanguageOptionButton.php';?>
+	<h1>Photograghy Gallery</h1>
+	<hr/>
+	<div class="albumarea">
+
+		<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_3453.JPG"></a></div>
+		<h2>Spring</h2>
+		</div>
+        
+        	<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_4885.JPG"></a></div>
+		<h2>Summer</h2>
+		</div>
+		
+
+		<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_0253.JPG"></a></div>
+		<h2>Autumn</h2>
+		</div>
+    		
+        
+        	<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_2017.JPG"></a></div>
+		<h2>Winter</h2>
+		</div>
+
+    		<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_0255.JPG"></a></div>
+		<h2>Morning</h2>
+		</div>
+
+		<div class = "album">
+		<div class = "pic tilt"><a href=""><img src = "Favorite/IMG_3523.JPG"></a></div>
+		<h2>Dawn</h2>
+		</div>
+
+    	<hr/>
+</div>
+	
+	<?php include '../../php/ExternalLinks.php';?>
+</center>
+
+<?php include '../../php/footer.php';?>
+
+</body>
+</html>
